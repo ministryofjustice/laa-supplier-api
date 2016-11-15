c134fa898e007c3386b686592120ecba6ec13a17
Josh Rowe <josh@s-block.com>
2016-11-15 20:27:17 +0000

    update static url for django 1.10

--- supplier_api/urls.py
+++ supplier_api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
 
 from moj_irat.views import PingJsonView, HealthcheckView
 
@@ -30,6 +31,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        url(r'^static/(?P<path>.*)$', serve,
             {'document_root': settings.STATIC_ROOT}),
     ]
